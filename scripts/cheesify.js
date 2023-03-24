ae18a39e58df3851684bdeb10b141d6d3957a2fb
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-24 21:48:48 +0900

    complete settings.to fix

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,9 +1,16 @@
-function cheesify() {
-  // TODO: Add the image replacement script here
-  images = document.querySelectorAll('img')
-  images.forEach((img)=>{
-    img.src = `https://source.unsplash.com/featured/?cheese/${img.width}x${img.height}?
-        ${Math.random()}`
-    img.srcset = img.src })
+// cheesify.js
 
+// Listen for messages on the content page
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === 'cheesify') cheesify();
+  }
+);
+
+// Our image replacement script
+function cheesify() {
+  document.querySelectorAll('img').forEach( (img) => {
+    img.src = `https://source.unsplash.com/${img.width}x${img.height}/?cheese&${Math.random()}`;
+    img.srcset = img.src;
+  })
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,14 @@
+// Send a message to the active tab to 'cheesify' it
 function sendCheesifyMsg() {
-  // TODO: Write a function to send a message to the active tab to 'cheesify' it
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // Finds tabs that are active in the current window
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'cheesify' }); // Sends a message (object) to the first tab (tabs[0])
+  });
 }
 
+// // Trigger the function above when clicking the 'Cheesify' button
+// document.querySelector('#cheesify').addEventListener('click', event => sendCheesifyMsg());
+
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
+document.querySelector('#cheesify').addEventListener('click', event => {
+  alert('you cliked');
+})
